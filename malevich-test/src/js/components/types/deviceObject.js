4b6766486939e7f40127b4762ca20900cfd42c10
PHOENIXIT\ekanarskaya <ekanarskaya@fil-it.ru>
2024-10-01 16:57:26 +0300

    new calc

--- malevich-test/src/js/components/types/deviceObject.js
+++ malevich-test/src/js/components/types/deviceObject.js
@@ -1,10 +1,17 @@
 /**
+ * @typedef DeviceDiscounts
+ * @prop {number} id
+ * @prop {string} minutes
+ * @prop {number} gb
+ */
+
+/**
  * @typedef DeviceObject
  * @prop {number} id
  * @prop {string} type
  * @prop {string} name
  * @prop {string} imageSrc
- * @prop {number[]} discountsArray
+ * @prop {DeviceDiscounts[]} discountsArray
  * @prop {number} maxDiscount
  * @prop {number} [defaultDiscount]
  * @prop {number} basePrice
